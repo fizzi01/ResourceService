9431dcf82b917f4bcab8e0dee41dc0aa8e51b065
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-04-24 17:35:11 +0200

    Initial commit

---
+++ src/test/java/it/unisalento/pasproject/memberservice/MemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisalento.pasproject.memberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
